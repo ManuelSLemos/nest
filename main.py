977a12f65fb684b547b343135aa26536e04df34c
Manuel S. Lemos <manuelslemos@gmail.com>
2023-12-25 19:48:52 +0000

    fix(@nestpy/common): :bug: syntax error in param nest application

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from packages.core.nest_application import NestAplication
 def bootstrap():
     app = NestAplication()
 
-    app.listen(3000)
+    app.listen( host='0.0.0.0', port=3000 )
 
 if __name__ == "__main__":
     bootstrap()
--- packages/common/interfaces/nest_application_interface.py
+++ packages/common/interfaces/nest_application_interface.py
@@ -3,7 +3,7 @@ from abc import ( ABC, abstractmethod )
 class INestAplication(ABC):
 
     @abstractmethod
-    def listen(self, host: str = '0.0.0.0' port: int = 8080) -> None:
+    def listen(self, host: str = '0.0.0.0', port: int = 8080) -> None:
         """Starts the application.
 
         Parameters
