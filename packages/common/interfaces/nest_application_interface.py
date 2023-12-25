1a07571241d1acc792e5c5fbdb921c5172458938
Manuel S. Lemos <manuelslemos@gmail.com>
2023-12-25 19:38:42 +0000

    feat(@nestpy/common): :building_construction: create interface for NestApplication

---
+++ packages/common/interfaces/nest_application_interface.py
@@ -0,0 +1,19 @@
+from abc import ( ABC, abstractmethod )
+
+class INestAplication(ABC):
+
+    @abstractmethod
+    def listen(self, host: str = '0.0.0.0' port: int = 8080) -> None:
+        """Starts the application.
+
+        Parameters
+        ----------
+
+        port : int, optional
+        host : str, optional
+    
+        Returns
+        -------
+
+        """    
+        pass
