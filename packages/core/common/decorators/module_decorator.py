8088006cc8d52802e0f713175bc714550f954faf
Manuel S. Lemos <manuelslemos@gmail.com>
2023-12-29 10:08:18 +0100

    refactor(@nestpy/core): :recycle: refactor func decorator to class decorator

--- packages/core/common/decorators/module_decorator.py
+++ packages/core/common/decorators/module_decorator.py
@@ -1,19 +1,29 @@
 from typing import ( Any, List )
 
-def Module(
-    imports: List[Any] = [],
-    controllers: List[Any] = [],
-    providers: List[Any] = [],
-    exports: List[Any] = []
-):
-    def decorator(ClassBasedView):
+class Module:
+    def __init__(
+        self, 
+        imports: List[Any] = [], 
+        controllers: List[Any] = [], 
+        providers: List[Any] = [], 
+        exports: List[Any] = []
+    ):
+        self.imports = imports
+        self.controllers = controllers
+        self.providers = providers
+        self.exports = exports
+
+    def __call__(decorator, ClassBasedView):
         class IModule(ClassBasedView):
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
-                self.imports=imports
-                self.controllers=controllers
-                self.providers=providers
-                self.exports=exports
-                
+                self.imports = decorator.imports
+                self.controllers = decorator.controllers
+                self.providers = decorator.providers
+                self.exports = decorator.exports
+
+            def _module_attributes(classProperty: Any, attrName: str, decoratorProperty: Any):
+                return classProperty if hasattr(decorator, attrName) else decoratorProperty
+        
+        IModule.decorator = decorator
         return IModule
-    return decorator
