3c3ee73922b28ab08d806aa3c3c0b88e26cbac31
Manuel S. Lemos <manuelslemos@gmail.com>
2023-12-29 13:14:42 +0100

    feat(@nestpy/core): :sparkles: implement router fastapi in controller decorator

--- packages/core/common/decorators/controller_decorator.py
+++ packages/core/common/decorators/controller_decorator.py
@@ -1,21 +1,32 @@
+from fastapi import APIRouter
+
 from typing import ( Any, List )
+
 
 # TODO: Consider whether to implement host param
 class Controller:
     def __init__(
         self, 
-        path: str | List[str] = '/',
-        # host: str
+        prefix: str | List[str] = '/',
     ):
-        self.path = path
-        # self.host = host
+        self.prefix = prefix
 
     def __call__(decorator, ClassBasedView):
         class IController(ClassBasedView):
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
-                self.path = decorator.path
-                # self.host = decorator.host
+                self.prefix = decorator.prefix
+                self.router = APIRouter(prefix=self.prefix)
+                
+                cls = ClassBasedView
+
+                for attr_name in dir(cls):
+                    attr = getattr(cls, attr_name)
+                    if callable(attr) and hasattr(attr, "is_route"):
+                        prefix, method = getattr(attr, "route_info")
+                        print(prefix, method)
+                        self.router.add_api_route(prefix, attr, methods=[method])
+
 
             def _module_attributes(classProperty: Any, attrName: str, decoratorProperty: Any):
                 return classProperty if hasattr(decorator, attrName) else decoratorProperty
